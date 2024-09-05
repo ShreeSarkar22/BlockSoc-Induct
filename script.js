b8c400dfcb794c440559c3492ed9098e36fdce87
Shree Sarkar <149933978+ShreeSarkar22@users.noreply.github.com>
2024-09-05 11:52:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
             datasets: [{
                 label: 'Crypto Portfolio Distribution',
                 data: [bitcoinBalance, ethereumBalance, binanceBalance],
-                backgroundColor: ['#f39c12', '#8e44ad', '#2980b9'],
-                borderColor: ['#f39c12', '#8e44ad', '#2980b9'],
+                backgroundColor: ['#08D9D6', '#252A34', '#FF2E63'],
+                borderColor: ['#08D9D6', '#252A34', '#FF2E63'],
                 borderWidth: 1
             }]
         };
@@ -137,3 +137,63 @@ document.addEventListener("DOMContentLoaded", function () {
 
     updateChart();
 });
+
+async function fetchBitcoinPriceInINR() {
+    const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr';
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const bitcoinPriceINR = data.bitcoin.inr.toFixed(2); 
+
+        document.getElementById('bitcoin-price').innerHTML = `${bitcoinPriceINR}`;
+    } catch (error) {
+        console.error('Error fetching Bitcoin price in INR:', error);
+        document.getElementById('bitcoin-price').innerHTML = 'Error fetching price';
+    }
+}
+
+async function fetchEthereumPriceInINR() {
+    const url = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=inr';
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const ethereumPriceINR = data.ethereum.inr.toFixed(2);
+
+        document.getElementById('ethereum-price').innerHTML = `${ethereumPriceINR}`;
+    } catch (error) {
+        console.error('Error fetching Ethereum price in INR:', error);
+        document.getElementById('ethereum-price').innerHTML = 'Error fetching price';
+    }
+}
+
+async function fetchBinanceCoinPriceInINR() {
+    const url = 'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=inr';
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const binanceCoinPriceINR = data.binancecoin.inr.toFixed(2);
+
+        document.getElementById('binancecoin-price').innerHTML = `${binanceCoinPriceINR}`;
+    } catch (error) {
+        console.error('Error fetching Binance Coin price in INR:', error);
+        document.getElementById('binancecoin-price').innerHTML = 'Error fetching price';
+    }
+}
+
+function fetchAllPrices() {
+    fetchBitcoinPriceInINR();
+    fetchEthereumPriceInINR();
+    fetchBinanceCoinPriceInINR();
+}
+
+function initializePriceFetching() {
+    fetchAllPrices();
+    setInterval(fetchAllPrices, 900000);
+}
+
+window.onload = () => {
+    initializePriceFetching();
+};
